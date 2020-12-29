e8ffd72648ba18b1b186b10f5906c899e869c16a
seanconway93 <75175597+seanconway93@users.noreply.github.com>
2020-12-29 13:13:40 +0000

    Import world cup data

--- gitTest.py
+++ gitTest.py
@@ -1,1 +1,5 @@
 print("Hello World")
+print(2+2)
+import pandas as pd
+world_cups = pd.read_csv(r"C:\Users\Acer Swift 5\PycharmProjects\pythonProject\archive\worldcups.csv")
+print(world_cups.head())
