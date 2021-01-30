43e24c11473fff39a4577ee717680b3514d3e4e7
seanconway93 <75175597+seanconway93@users.noreply.github.com>
2021-01-30 23:40:02 +0000

    final committ and push

--- UCDproject.py
+++ UCDproject.py
@@ -55,9 +55,10 @@ ax.set(xlabel= "Year", ylabel = "Average Goals Per Game")
 ax.set_xticks(years, minor=True)
 ax.grid()
 plt.xlim(1930, 2014)
-#plt.show()
+plt.show()
 
 #Goals per tournament over time
+
 sns.set_style("darkgrid")
 sns.catplot(x="Year", y="GoalsScored", data=world_cups,
             kind="point", hue="GoalsScored", palette=sns.color_palette("flare", n_colors=17))
@@ -65,23 +66,21 @@ plt.xticks(rotation=55)
 plt.show()
 
 #Tournaments with average goals per game of 3 or higher
+
 gpgyears = pd.concat([gpg, years], axis=1)
 avg_goals_loc = gpgyears[gpgyears.iloc[:,0]>= 3]
 avg_goals_loc = avg_goals_loc.round(decimals=1)
 print(avg_goals_loc)
 
+#World Cup Finals
+
 finals = matches[(matches["Stage"]=="Final")]
 finals = finals.loc[:, ["Year", "Home Team Name", "Home Team Goals", "Away Team Name", "Away Team Goals"]]
 finals = finals.drop_duplicates()
-#print(finals)
+print(finals)
 
 finals_world_cups = world_cups.merge(finals, on="Year")
-#print(finals_world_cups.head())
 finals_goals = finals_world_cups["Home Team Goals"] + finals_world_cups["Away Team Goals"]
 finals_goals = finals_goals.astype(int)
 merged_years = finals_world_cups["Year"]
 goals_per_final = pd.concat([merged_years, finals_goals], axis=1)
-
-#goals_per_final = pd.concat([finals_goals, years], axis=1)
-print(goals_per_final)
-#gpgyears = pd.concat([gpg, years], axis=1)
